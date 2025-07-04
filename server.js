d1e83c1add1cbe0703c2a105d629e2922c96450a
Ovindu Pathiraja <cb010931@students.apiit.lk>
2025-07-04 06:24:35 +0530

    first

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
+import connectDB from './configs/db.js';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+await connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
+
+app.get('/', (req, res) => res.send("API is working"));
+
+export const handler = serverless(app);
